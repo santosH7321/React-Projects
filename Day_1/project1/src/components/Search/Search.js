670c0696e892470bc182de2fd0da6265cbf679f4
Santosh Kumar <santoshkumar23kky@gmail.com>
2023-12-15 10:19:19 +0530

    Day_1

--- Day_1/project1/src/components/Search/Search.js
+++ Day_1/project1/src/components/Search/Search.js
@@ -1,7 +1,7 @@
 function Search(){
     return(
         <div className="flex items-center m-5 p-5 px-5 border-solid border-2 border-sky-500 rounded-lg bg-gray-900 shadow-2xl drop-shadow-2xl hover:bg-pink-400  ">
-        <input className="w-[100%] p-3 rounded "
+        <input className="w-[100%] p-3 rounded border-y-4 border-yellow-400"
             type="text"
             placeholder="pokemon name...."
         />
