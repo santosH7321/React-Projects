eb827872a27cec9145fd2092b262f8c9d8c98f3b
Santosh Kumar <santoshkumar23kky@gmail.com>
2023-12-18 19:21:08 +0530

    DiceGame

--- Day_5/dice-game/src/components/NumberSelector.jsx
+++ Day_5/dice-game/src/components/NumberSelector.jsx
@@ -2,20 +2,22 @@ import { useState } from "react";
 
 
 const NumberSelector = () => {
+    const arrNumber = [1,2,3,4,5,6];
 
     const [selectedNumber, setSelectedNumber] = useState();
 
   return (
     <div>
+      {arrNumber.map((value, i) => {
+        <div>
+          isSelected = {value === selectedNumber}
+          key = {i}
+          onClick={() => setSelectedNumber(value)}
+          {value}
+        </div>
+      })}
         <div className="flex py-10">
-            <p className="h-[72px] w-[72px] border-4 border-black text-5xl font-bold flex justify-center ">1</p>
-            <p className="h-[72px] w-[72px] border-4 border-black text-5xl font-bold flex justify-center ">2</p>
-            <p className="h-[72px] w-[72px] border-4 border-black text-5xl font-bold flex justify-center ">3</p>
-            <p className="h-[72px] w-[72px] border-4 border-black text-5xl font-bold flex justify-center ">4</p>
-            <p className="h-[72px] w-[72px] border-4 border-black text-5xl font-bold flex justify-center ">5</p>
-            <p className="h-[72px] w-[72px] border-4 border-black text-5xl font-bold flex justify-center ">6</p>
-
-            
+        
         </div>
     </div>
   );
