527e3aebfb98433159ac51406dcb7e5ac9d03940
Santosh Kumar <santoshkumar23kky@gmail.com>
2023-12-25 12:00:22 +0530

    useState

---
+++ Day_9/js/app/page.js
@@ -0,0 +1,24 @@
+
+"use client"
+import React, { useState } from 'react'
+
+const page = () => {
+  const [marks, setmarks] = useState(80)
+  const [gretting, setgretting] = useState("Hello Namaste jee");
+  return (
+    <div>
+      <h1 className='text-2xl font-bold text-sky-600 '>My marks is {marks}</h1>
+      <button onClick={() => {
+        setmarks(50)
+      }} className='px-5 py-2 bg-black rounded text-white font-bold m-5 '>Update</button>
+
+
+      <h1 className='text-2xl font-bold text-sky-600 '>{gretting}</h1>
+      <button onClick={() => {
+        setgretting("Kaise hai aap")
+      }} className='px-5 py-2 bg-black rounded text-white font-bold m-5 '>Update</button>
+    </div>
+  )
+}
+
+export default page
