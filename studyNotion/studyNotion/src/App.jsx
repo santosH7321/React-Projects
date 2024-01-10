cbfd8a4fe369c6aa63fef139f2f54157c1d55276
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-01-10 15:29:54 +0530

    Route

--- studyNotion/studyNotion/src/App.jsx
+++ studyNotion/studyNotion/src/App.jsx
@@ -1,13 +1,25 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+import { useState } from 'react';
 
 function App() {
-
+const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <>
-     <Navbar />
+     <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
+     <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='login' element={<Login/>} />
+      <Route path='signup' element={<Signup />} />
+      <Route path='dashboard' element={<Dashboard />} />
+     </Routes>
     </>
   )
 }
---
+++ studyNotion/studyNotion/src/components/Navbar.jsx
@@ -0,0 +1,55 @@
+
+import { Link } from 'react-router-dom';
+import Logo from '../assets/Logo.svg';
+
+
+const Navbar = (props) => {
+  let isLoggedIn = props.isLoggedIn;
+  let setIsLoggedIn = props.setIsLoggedIn;
+  return (
+    <div className='flex justify-around items-center '>
+      <Link to="/">
+        <img src={Logo} alt='logo' width={160} height={32} loading='lazy'/>
+      </Link>
+
+      <nav>
+        <ul className='flex gap-5'>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+          {/* Login signup logout Dashboard */}
+      <div className='flex gap-7'>
+        { !isLoggedIn &&
+          <Link to='/login'>
+              <button>Login</button>
+          </Link>
+        }
+        { !isLoggedIn &&
+          <Link to='/signup'>
+              <button>Sign Up</button>
+          </Link>
+        }
+        { isLoggedIn &&
+          <Link to='/'>
+              <button>Log Out</button>
+          </Link>
+        }
+        { isLoggedIn &&
+          <Link to='/dashboard'>
+              <button>Dashboard</button>
+          </Link>
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
