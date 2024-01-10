f99baf5fc76308fcac23dc818bd816b73bcdc8c7
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-01-10 21:52:15 +0530

    Navbar

--- E-Commerce/E-Commerce/src/App.jsx
+++ E-Commerce/E-Commerce/src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from "./Components/Hero/Hero"
 import Navbar from "./Components/Navbar/Navbar"
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Hero />
     </div>
   )
 }
--- E-Commerce/E-Commerce/src/Components/Navbar/Navbar.jsx
+++ E-Commerce/E-Commerce/src/Components/Navbar/Navbar.jsx
@@ -81,7 +81,7 @@ const Navbar = ({ handleOrderPopup }) => {
               onClick={() => handleOrderPopup()}
               className="bg-gradient-to-r from-primary to-secondary transition-all duration-200 text-white  py-1 px-4 rounded-full flex items-center gap-3 group"
             >
-              <span className="group-hover:block hidden transition-all duration-200">
+              <span className="group-hover:block hidden transition-all duration-200 hover:delay-200">
                 Order
               </span>
               <FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer" />
