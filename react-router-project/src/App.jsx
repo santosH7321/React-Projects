4766a40c0a57a575fde0a3dbbbe4acc7069cef19
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-01-13 14:56:29 +0530

    footerAndHome

--- react-router-project/src/App.jsx
+++ react-router-project/src/App.jsx
@@ -1,6 +1,8 @@
 
 import './App.css'
+import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
+import Home from './components/Home/Home'
 
 function App() {
 
@@ -8,6 +10,8 @@ function App() {
   return (
     <>
       <Header/>
+      <Home />
+      <Footer />
     </>
   )
 }
