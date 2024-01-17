d252b1043d8e2e1442932eb242b41346662aca40
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-01-17 13:17:46 +0530

    SomeChanges

--- 3d_portfolio/src/Pages/Home.jsx
+++ 3d_portfolio/src/Pages/Home.jsx
@@ -1,10 +1,28 @@
-
-
+import { Canvas } from "@react-three/fiber"
+import { Suspense } from "react"
+import Loader from "../components/Loader"
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <section className="w-full h-screen relative">
+      {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        POPUP
+      </div> */}
+
+    <Canvas className="w-full h-screen relative"
+    camera={{near: 0.1, far: 1000}}>
+
+      <Suspense fallback={<Loader />}>
+        <directionalLightShadow />
+        <ambientLight />
+        <pointLight />
+        <spotLight />
+        <hemisphereLight />
+      </Suspense>
+    </Canvas>
+
+      
+      
+    </section>
   )
 }
 
