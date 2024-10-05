aab81e1e90388018b092ef6abd566448ab92a392
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-10-05 10:30:50 +0530

    nasted div with parent child relation

---
+++ NamasteReact/Lecture01/app.js
@@ -0,0 +1,34 @@
+// Using JavaScript
+// const data = document.createElement("h1");
+// data.innerHTML = "Welcome to React";
+// document.getElementById("head").appendChild(data);
+
+/*
+<div id="parent">
+    <div id="child">
+        <h1>I am h1 tag</h1>
+        <h2>I am h2 tag</h2>
+    </div>
+</div>
+*/
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ])
+);
+
+// Using React
+const heading = React.createElement(
+  "h1",
+  { id: "heading", xyz: "abc" },
+  "Hello Jee Kaise hai app"
+);
+
+console.log(parent); // Object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
