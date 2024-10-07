00438fb7f2fc5c0d2eb2ba6020425906088e7a53
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-10-07 15:25:21 +0530

    Lecture3

---
+++ NamasteReact/Lecture03/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const heading = React.createElement("h1", {id: "heading"}, "Namaste React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+
