3eec63f2800fe284a6471f722ec9c33d6dc6cb19
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-10-07 16:50:39 +0530

    components

--- NamasteReact/Lecture03/app.js
+++ NamasteReact/Lecture03/app.js
@@ -1,9 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
-const heading = React.createElement("h1", {id: "heading"}, "Namaste React");
+const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
 
+// React Element
+// const jsxHeading = (
+//   <h1 className="head" tabIndex="1">
+//     Hello jee i'm jsx but not HTML? I am like HTML 😃
+//   </h1>
+// );
+
+const JsxHeading = () => (
+  <h1 className="head" tabIndex="1">
+    Hello jee i'm jsx but not HTML? I am like HTML 😃
+  </h1>
+);
+
+const Title = () => (
+  <div>
+    <h1 className="title">Namaste React</h1>
+    {/* Three ways to call components */}
+    <JsxHeading />
+    {JsxHeading()}
+    <JsxHeading></JsxHeading>
+  </div>
+);
+root.render(<JsxHeading />);
