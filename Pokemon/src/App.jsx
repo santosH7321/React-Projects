27e180ca0d9d5f77c128c427585a7fc6692165cc
Santosh Kumar <santoshkumar23kky@gmail.com>
2025-04-29 08:21:30 +0530

    Project Setup

---
+++ Pokemon/src/App.jsx
@@ -0,0 +1,12 @@
+import Pockemon from "./components/Pockemon";
+
+
+const App = () => {
+  return (
+    <div>
+     <Pockemon />
+    </div>
+  )
+}
+
+export default App;
---
+++ Pokemon/src/components/Pockemon.jsx
@@ -0,0 +1,11 @@
+
+
+import React from 'react'
+
+const Pockemon = () => {
+  return (
+    <div>Pockemon</div>
+  )
+}
+
+export default Pockemon
