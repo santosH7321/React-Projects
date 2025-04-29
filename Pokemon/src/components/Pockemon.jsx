5e16f52f275ee2070380ceaf88d70d3efb908584
Santosh Kumar <santoshkumar23kky@gmail.com>
2025-04-29 08:49:26 +0530

    Pokemon api fetch response

--- Pokemon/src/components/Pockemon.jsx
+++ Pokemon/src/components/Pockemon.jsx
@@ -1,10 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import "../index.css"
 
+const Pockemon = () => { 
 
-import React from 'react'
+    const [pokemon, setPokemon] = useState([]); 
 
-const Pockemon = () => {
+    const API = "https://pokeapi.co/api/v2/pokemon?limit=150";
+
+    const fetchPockemon = async () => {
+        try {
+            const res = await fetch(API);
+            const data = await res.json();
+            // console.log(data);
+
+            const detailedPokemonData = data.results.map( async (curPokemon) => {
+                const res = await fetch(curPokemon.url);
+                const data = await res.json();
+                return data;
+            })
+        
+
+            const detailedResponses = await Promise.all(detailedPokemonData);
+            console.log(detailedResponses)
+            setPokemon(detailedResponses);
+        } catch (error) {
+            console.log("Some problem in api", error)
+        }
+    }
+    useEffect(() => {
+        fetchPockemon();
+    }, []);
   return (
-    <div>Pockemon</div>
+    <div className='flex items-center text-5xl font-bold'>Pockemon</div>
   )
 }
 
