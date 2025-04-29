debc681b5b5b67926df4998bcd2a1cfb8a116d13
Santosh Kumar <santoshkumar23kky@gmail.com>
2025-04-29 10:21:04 +0530

    Pokemon all field

--- Pokemon/src/components/PockemonCards.jsx
+++ Pokemon/src/components/PockemonCards.jsx
@@ -14,6 +14,36 @@ const PockemonCards = ({ pokemonData }) => {
       <div className="pokemon-info pokemon-highlight">
         <p>{pokemonData.types.map((curType) => curType.type.name).join(", ")}</p>
       </div>
+      <div className="grid-three-cols">
+        <p className="pokemon-info">
+            <span>Height:</span>{pokemonData.height}
+        </p>
+        <p className="pokemon-info">
+            <span>Weight:</span>{pokemonData.weight}
+        </p>
+        <p className="pokemon-info">
+            <span>speed:</span>{pokemonData.stats[5].base_stat}
+        </p>
+      </div>
+      <div className="grid-three-cols">
+        <div className="pokemon-info">
+          <p>{pokemonData.base_experience}</p>
+          <span> Experience:</span>
+        </div>
+        <div className="pokemon-info">
+          <p>{pokemonData.stats[1].base_stat}</p>
+          <span>Attack:</span>
+        </div>
+        <div className="pokemon-info">
+          <p>
+            {pokemonData.abilities
+              .map((abilityInfo) => abilityInfo.ability.name)
+              .slice(0, 1)
+              .join(", ")}
+          </p>
+          <span> Abilities: </span>
+        </div>
+      </div>
     </li>
   );
 };
